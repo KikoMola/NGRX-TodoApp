bd1f0f23815738a9f9d3767b986579c02fd57998
Kiko <kikoguirado@gmail.com>
2022-07-27 12:22:10 +0200

    added filters

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducer';
+import { filtrosValidos } from 'src/app/filtro/filtro.actions';
 
 import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
-
   todos: Todo[] = [];
+  filtroActual!: filtrosValidos;
 
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.store.select('todos').subscribe(todos => this.todos = todos);
+    // this.store.select('todos').subscribe(todos => this.todos = todos);
+    this.store.subscribe((state) => {
+      this.todos = state.todos;
+      this.filtroActual = state.filtro;
+    });
   }
-
 }
